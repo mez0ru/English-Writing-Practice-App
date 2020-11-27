ef9088770deaa8c03e40ae2afbceeeae18aa806d
mez0ru <mez0ru@gmail.com>
2020-11-27 22:09:58 +0800

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {DemoMaterialModule} from './material-module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    DemoMaterialModule,
+    BrowserAnimationsModule
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  exports: [DemoMaterialModule]
 })
-export class AppModule { }
+export class AppModule {}
